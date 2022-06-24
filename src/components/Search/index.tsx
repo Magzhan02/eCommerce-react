8f07b4fb2d1a19dbac64bd5135fe188bd5e50db1
Magzhan <sunmorring@gmail.com>
2022-06-24 19:46:28 +0600

    добавил адаптивку к search components

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -6,6 +6,10 @@ import { SearchContext } from '../../App';
 const Search = () => {
   const { searchValue, setSearchValue } = SearchContext();
 
+  const clearSearchValue = () => {
+    setSearchValue('');
+  };
+
   return (
     <div className={styles.searchBlock}>
       <svg viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" className={styles.icon}>
@@ -20,9 +24,15 @@ const Search = () => {
         onChange={(event) => setSearchValue(event.target.value)}
         value={searchValue}
       />
-      <svg viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" className={styles.clear}>
-        <path d="M10 8.586L2.929 1.515 1.515 2.929 8.586 10l-7.071 7.071 1.414 1.414L10 11.414l7.071 7.071 1.414-1.414L11.414 10l7.071-7.071-1.414-1.414L10 8.586z" />
-      </svg>
+      {searchValue && (
+        <svg
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+          className={styles.clear}
+          onClick={() => clearSearchValue()}>
+          <path d="M10 8.586L2.929 1.515 1.515 2.929 8.586 10l-7.071 7.071 1.414 1.414L10 11.414l7.071 7.071 1.414-1.414L11.414 10l7.071-7.071-1.414-1.414L10 8.586z" />
+        </svg>
+      )}
     </div>
   );
 };
